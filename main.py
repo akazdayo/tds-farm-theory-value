3c01484b201e503005947e9913e58b8b07cd27c2
akaz <82073147+akazdayo@users.noreply.github.com>
2023-07-29 23:07:10 +0900

    bug fix

--- main.py
+++ main.py
@@ -3,14 +3,14 @@ import json
 
 def get_money(wave, players, farms):
     wave_bonus = (180 + (50 * wave)) // players  # Fallen
-    # wave_bonus = (180 + (100 * wave)) / players #Normal
+    # wave_bonus = (180 + (100 * wave)) // players #Normal
     farms = [level[i][1] for i in farms]
     farm_bonus = sum(farms)
     all_bonus = wave_bonus + farm_bonus
-    # print(all_bonus)
     return all_bonus
 
-player = 3
+
+player = 2
 max_wave = 13
 max_level = 5
 max_farm = 8
@@ -24,12 +24,12 @@ for current_wave in range(max_wave+1):
         for current_farm in range(max_farm):
             money = 600
             farm = []
-            graph = {i: {"first_money":0, "money": 0, "spend": 0, "farm": []} for i in range(current_wave+1)}
+            graph = {i: {"first_money": 0, "money": 0, "spend": 0, "farm": []} for i in range(current_wave+1)}
             for w in range(current_wave+1):
                 if w != 0:
                     money += get_money(w, player, farm)
                 graph[w]["first_money"] = money
-                
+
                 if len(farm) < current_farm+1:  # place
                     for i in range((current_farm+1) - len(farm)):
                         if money >= 250:
@@ -42,8 +42,8 @@ for current_wave in range(max_wave+1):
                         for i in range(current_level+1):
                             if i in farm and money >= level[i][0]:
                                 farm[farm.index(i)] += 1
-                                money -= level[i][0]
-                                graph[w]["spend"] -= level[i][0]
+                                money -= level[i+1][0]
+                                graph[w]["spend"] -= level[i+1][0]
                 graph[w]["money"] = money
                 graph[w]["farm"] = farm.copy()
 
