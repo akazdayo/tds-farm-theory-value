f8a06a91414f6dd584ad828e91290231c7c3bd94
akaz <82073147+akazdayo@users.noreply.github.com>
2023-07-29 14:49:46 +0900

    Save json

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import json
+
+
 def get_money(wave, players, farms):
-    wave_bonus = (180 + (50 * wave)) / players  # Fallen
+    wave_bonus = (180 + (50 * wave)) // players  # Fallen
     # wave_bonus = (180 + (100 * wave)) / players #Normal
     farms = [level[i][1] for i in farms]
     farm_bonus = sum(farms)
@@ -9,8 +12,8 @@ def get_money(wave, players, farms):
 
 
 level = {0: [250, 50], 1: [200, 100], 2: [550, 250], 3: [1000, 500], 4: [2500, 750], 5: [5000, 1500]}
-player = 1
-highest = {"money": 0, "wave": 0, "level": 0, "graph": None}
+player = 4
+highest = {"player": player, "difficulty": "Fallen", "money": 0, "wave": 0, "level": 0, "graph": None}
 """
 money = 600
 farm = []
@@ -25,7 +28,7 @@ for max_wave in range(13):
         for max_farm in range(8):
             money = 600
             farm = []
-            graph = {i: {"money": 0, "spend": 0, "farm": []} for i in range(max_wave+1)}
+            graph = {i+1: {"money": 0, "spend": 0, "farm": []} for i in range(max_wave+1)}
             for w in range(max_wave+1):
                 money += get_money(w, player, farm)
                 if len(farm) < max_farm+1:  # place
@@ -33,7 +36,7 @@ for max_wave in range(13):
                         if money >= 250:
                             farm.append(0)
                             money -= 250
-                            graph[w]["spend"] -= 250
+                            graph[w+1]["spend"] -= 250
 
                 else:  # upgrade
                     for j in range(max_farm+1):
@@ -42,16 +45,15 @@ for max_wave in range(13):
                                 farm[farm.index(i)] += 1
                                 money -= level[i][0]
                                 graph[w]["spend"] -= level[i][0]
-                graph[w]["money"] = money
-                graph[w]["farm"] = farm.copy()
+                graph[w+1]["money"] = money
+                graph[w+1]["farm"] = farm.copy()
 
             if highest["money"] < money:
                 highest["money"] = money
                 highest["wave"] = max_wave+1
                 highest["level"] = max_level+1
                 highest["graph"] = graph
-                #print(max_wave+1,max_level+1 ,max_farm+1,money,farm,graph)
-                #highest = {"money":0,"wave":0,"level":0,"graph":None}
 
 
-print(highest)
+with open("data/farm.json", "w") as f:
+    json.dump(highest, f, indent=4)
